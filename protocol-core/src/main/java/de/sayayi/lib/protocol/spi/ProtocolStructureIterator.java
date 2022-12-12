05726e35002558a7e751f6f672a8e684b0920230
jeroen.gremmen <jeroen.gremmen@sayayi.de>
2022-12-12 22:29:30 +0100

    implement GroupMessageEntry#getThrowable as a default interface method

--- protocol-core/src/main/java/de/sayayi/lib/protocol/spi/ProtocolStructureIterator.java
+++ protocol-core/src/main/java/de/sayayi/lib/protocol/spi/ProtocolStructureIterator.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *   https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -485,13 +485,6 @@ abstract class ProtocolStructureIterator<M> implements ProtocolIterator<M>
     @Override
     public @NotNull Map<String,Object> getParameterValues() {
       return groupMessage.getParameterValues();
-    }
-
-
-    @Override
-    @Contract(value = "-> null", pure = true)
-    public Throwable getThrowable() {
-      return null;
     }
 
 
